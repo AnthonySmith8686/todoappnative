bfc8331a42e05b76bf7f1d2a93b38545bc966682
AnthonySmith8686 <anthonysmith8686@gmail.com>
2019-02-18 00:35:14 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import Main from '.app/component/Main';
+
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <Main />
+
+    );
+  }
+}
